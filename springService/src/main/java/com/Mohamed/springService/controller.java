41d473554415090a38eb122670b6002905e99826
Mohamed <ahnbi32@gmail.com>
2023-11-11 21:48:37 +0100

    Establishment of connection and exchange of data from spring service to nodejs service using ResteTemplate

--- springService/src/main/java/com/Mohamed/springService/controller.java
+++ springService/src/main/java/com/Mohamed/springService/controller.java
@@ -2,9 +2,12 @@ package com.Mohamed.springService;
 
 import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class controller {
@@ -16,7 +19,9 @@ public class controller {
     }
 
 
-    /**OpenFeign**/
+    /**
+     * Using OpenFeign
+     **/
     @Autowired
     private NodeFeignClient nodeFeignClient;
 
@@ -26,8 +31,25 @@ public class controller {
     }
 
     /**
-    * Avec OpenFeign, vous n'avez pas à écrire manuellement la logique de l'appel HTTP, cela est géré par la bibliothèque.
-    * Cela rend le code plus propre et plus lisible. Notez que NodeFeignClient est une interface, et Feign génère automatiquement
-    * une implémentation pour vous. Assurez-vous simplement que le nom de votre service correspond à celui défini dans l'annotation @FeignClient.*/
+     * Avec OpenFeign, vous n'avez pas à écrire manuellement la logique de l'appel HTTP, cela est géré par la bibliothèque.
+     * Cela rend le code plus propre et plus lisible. Notez que NodeFeignClient est une interface, et Feign génère automatiquement
+     * une implémentation pour vous. Assurez-vous simplement que le nom de votre service correspond à celui défini dans l'annotation @FeignClient.*/
 
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     * Using RestTemplate
+     **/
+
+    @PostMapping("/spring/sendDataToNodeV1")
+    public ResponseEntity<String> sendDataToNodeV1(@RequestBody product teste) {
+        product dataToSend = teste;
+        String nodeEndpointUrl = "http://localhost:8001/node/spring-data";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(nodeEndpointUrl, dataToSend, String.class);
+
+        String nodeResponse = responseEntity.getBody();
+        return ResponseEntity.ok("Spring send data. Node.js response: " + nodeResponse);
+    }
+    /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 }
